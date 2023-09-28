adb3a0921d529587188f1cd89cb872fa1f90182d
Tonkel <w.tucker.cac@gmail.com>
2023-09-28 11:40:13 -0400

    adds all files/work

---
+++ Develop/models/index.js
@@ -0,0 +1,25 @@
+const location = require("./location");
+const Traveller = require("./traveller");
+const Trip = require("./trip");
+
+Traveller.belongsToMany(Location, {
+  //define table
+  through: {
+    model: Trip,
+    unique: false,
+  },
+  //define alias for includes
+  as: "planned_trips",
+});
+
+Location.belongsToMany(Traveller, {
+  //define table
+  through: {
+    model: Trip,
+    unique: false,
+  },
+  //define alias for includes
+  as: "places_travelled",
+});
+
+module.exports = { Reader, Book, LibraryCard };
